79b595fdeba4ed7de996ac34c498f976de233784
dymafr <erwan1309@hotmail.com>
2021-01-02 19:15:52 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-chap12-l10
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-chap12-l10)
---
+++ src/app/app.component.ts
@@ -0,0 +1,48 @@
+import { Component, OnInit } from "@angular/core";
+import {
+  FormArray,
+  FormBuilder,
+  FormControl,
+  FormGroup,
+  Validators
+} from "@angular/forms";
+
+@Component({
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.scss"]
+})
+export class AppComponent implements OnInit {
+  public form: FormGroup;
+
+  constructor(private fb: FormBuilder) {}
+
+  get hobbies() {
+    return this.form.get("hobbies") as FormArray;
+  }
+
+  ngOnInit() {
+    this.createForm();
+  }
+
+  createForm() {
+    this.form = this.fb.group({
+      nom: ["", Validators.required],
+      email: ["", [Validators.required, Validators.email]],
+      hobbies: this.fb.array([]),
+      password: [""]
+    });
+  }
+
+  addHobby() {
+    this.hobbies.push(new FormControl(""));
+  }
+
+  deleteHobby(index: number) {
+    this.hobbies.removeAt(index);
+  }
+
+  submit() {
+    console.log(this.form.value);
+  }
+}
