2201e33f5d60f547bb2af01933b748f80a35bfbc
dymafr <erwan1309@hotmail.com>
2021-08-16 16:43:32 +0200

    1 file modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,41 +1,27 @@
-import { Component, OnInit } from "@angular/core";
-import {
-  FormArray,
-  FormBuilder,
-  FormControl,
-  FormGroup,
-  Validators
-} from "@angular/forms";
+import { Component } from '@angular/core';
+import { FormArray, FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
-  selector: "app-root",
-  templateUrl: "./app.component.html",
-  styleUrls: ["./app.component.scss"]
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
 })
-export class AppComponent implements OnInit {
-  public form: FormGroup;
+export class AppComponent {
+  public form: FormGroup = this.fb.group({
+    nom: ['', Validators.required],
+    email: ['', [Validators.required, Validators.email]],
+    hobbies: this.fb.array([]),
+    password: ['']
+  });
 
   constructor(private fb: FormBuilder) {}
 
   get hobbies() {
-    return this.form.get("hobbies") as FormArray;
-  }
-
-  ngOnInit() {
-    this.createForm();
-  }
-
-  createForm() {
-    this.form = this.fb.group({
-      nom: ["", Validators.required],
-      email: ["", [Validators.required, Validators.email]],
-      hobbies: this.fb.array([]),
-      password: [""]
-    });
+    return this.form.get('hobbies') as FormArray;
   }
 
   addHobby() {
-    this.hobbies.push(this.fb.control(""));
+    this.hobbies.push(this.fb.control(''));
   }
 
   deleteHobby(index: number) {
